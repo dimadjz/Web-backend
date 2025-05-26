7286c670709636e62e8d4e567c9ed844af97b4f7
dimadjz <dimaspavlov2025@gmail.com>
2025-05-26 19:29:42 +0300

    dunk

--- 7/config.php
+++ 7/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'u68763');
-define('DB_USER', 'u68763');
-define('DB_PASS', '7680994');
+define('DB_NAME', 'u68841');
+define('DB_USER', 'u68841');
+define('DB_PASS', '3842702');
 define('ADMIN_LOGIN', 'admin');
 define('ADMIN_PASS_HASH', password_hash('admin123', PASSWORD_BCRYPT));
 define('CSRF_TOKEN_LIFETIME', 3600);
